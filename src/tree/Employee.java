977346ea846fb138a866f6d99c155884c572838d
graysonhappy <gkamadeu@gmail.com>
2026-03-27 12:26:19 -0400

    init commit

--- src/tree/Employee.java
+++ src/tree/Employee.java
@@ -5,40 +5,33 @@ package tree;
  * The entity we're storing in our node
  */
 
+
 public class Employee implements Comparable<Employee> {
 	
 	private String name;
 	private Integer id;
 	private String position;
 	
- 	public String getName() {
+	public Employee(String name, Integer id, String position) {
+		this.name = name;
+		this.id = id;
+		this.position = position;
+	}
+ 	
+	public String getName() {
 		return name;
 	}
-
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
 
 	public Integer getId() {
 		return id;
 	}
-
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
 
 	public String getPosition() {
 		return position;
 	}
 
 
-	public void setPosition(String position) {
-		this.position = position;
-	}
+	
 
 	public boolean equals(Employee e) {
 		if (this.name.equals(e.name) && this.id == e.id && this.position.equals(e.position))
