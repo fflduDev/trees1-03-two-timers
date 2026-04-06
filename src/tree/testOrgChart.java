60b406c339cc17bb2795e3cb115202f668835264
graysonhappy <gkamadeu@gmail.com>
2026-04-06 13:21:50 -0400

    init commit

--- src/tree/testOrgChart.java
+++ src/tree/testOrgChart.java
@@ -32,20 +32,20 @@ public class testOrgChart {
 		company.addDirectReport(e7, e9);
 		
 		// show it depth first
-		company.showOrgChartDepthFirst();
+		// company.showOrgChartDepthFirst();
 		
 		// show breadth first
-		// company.showOrgChartBreadthFirst();
+		company.showOrgChartBreadthFirst();
 		
 		// and remove some people
-	//	company.removeEmployee(e4);
-	//	company.removeEmployee(e5);
+	company.removeEmployee(e4);
+	company.removeEmployee(e5);
 		
 		// show it depth first
 	//	company.showOrgChartDepthFirst();
 		
 		// show breadth first
-//		company.showOrgChartBreadthFirst();
+	company.showOrgChartBreadthFirst();
 		
 
 	}
